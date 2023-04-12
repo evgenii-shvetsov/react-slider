3baac57930f642e4ad4b8887c1eda526002cc93b
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-12 15:26:23 -0700

    build slider with doubly linked list

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import DoublyLinkedList from "./DoublyLinkedList"; 
+import React, { useState, useRef } from "react";
+import DoublyLinkedList from "./DoublyLinkedList";
+import "./Carousel.css";
 
 const Carousel = ({ slides }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [list, setList] = useState(new DoublyLinkedList());
 
-  
+
   useState(() => {
     const newList = new DoublyLinkedList();
     slides.forEach((slide) => {
@@ -14,31 +15,40 @@ const Carousel = ({ slides }) => {
     setList(newList);
   }, [slides]);
 
-const goToNextSlide = () => {
-  if (currentSlide < slides.length - 1) {
-    setCurrentSlide(currentSlide + 1);
-    if (list && list.next) {
-      setList(list.next());
+  const goToNextSlide = () => {
+    if (currentSlide < slides.length - 1) {
+      setCurrentSlide(currentSlide + 1);
+      if (list && list.next) {
+        setList(list.next());
+      }
     }
-  }
-};
+  };
 
-const goToPrevSlide = () => {
-  if (currentSlide > 0) {
-    setCurrentSlide(currentSlide - 1);
-    if (list && list.prev) {
-      setList(list.prev());
+  const goToPrevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+      if (list && list.prev) {
+        setList(list.prev());
+      }
     }
-  }
-};
+  };
+
+
 
   return (
     <div style={{ width: "800px", margin: "0 auto" }}>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <button onClick={goToPrevSlide}>Prev</button>
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={goToPrevSlide} className="prev-button">
+          Prev
+        </button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           {slides.slice(currentSlide, currentSlide + 3).map((slide, index) => (
-            <div key={index}>
+            <div key={index} className="slide">
               <img
                 src={slide.image}
                 alt={slide.title}
@@ -49,10 +59,12 @@ const goToPrevSlide = () => {
             </div>
           ))}
         </div>
-        <button onClick={goToNextSlide}>Next</button>
+        <button onClick={goToNextSlide} className="next-button">
+          Next
+        </button>
       </div>
     </div>
   );
 };
 
-export default Carousel
+export default Carousel;
