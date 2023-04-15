b0b4a80cce9416a698f21f006e8cda17e06d2503
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-14 21:00:13 -0700

    add small changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,20 +7,19 @@ const Carousel = ({ slides }) => {
   const [currentNode, setCurrentNode] = useState(null);
   const doublyLinkedList = useRef(new DoublyLinkedList());
 
-useEffect(() => {
-  slides.forEach((slide) => {
-    doublyLinkedList.current.appendNode(slide);
-  });
+  useEffect(() => {
+    slides.forEach((slide) => {
+      doublyLinkedList.current.appendNode(slide);
+    });
 
-  if (doublyLinkedList.current.head) {
-    doublyLinkedList.current.head.prev =
-      doublyLinkedList.current.head.prev || doublyLinkedList.current.tail;
-    doublyLinkedList.current.tail.next =
-      doublyLinkedList.current.tail.next || doublyLinkedList.current.head;
-    setCurrentNode(doublyLinkedList.current.head.next);
-  }
-}, [slides]);
-
+    if (doublyLinkedList.current.head) {
+      doublyLinkedList.current.head.prev =
+        doublyLinkedList.current.head.prev || doublyLinkedList.current.tail;
+      doublyLinkedList.current.tail.next =
+        doublyLinkedList.current.tail.next || doublyLinkedList.current.head;
+      setCurrentNode(doublyLinkedList.current.head.next);
+    }
+  }, [slides]);
 
   const goToNextSlide = async () => {
     if (isNavigating) return;
@@ -33,68 +32,67 @@ useEffect(() => {
     setIsNavigating(false);
   };
 
-const goToPrevSlide = () => {
-  if (isNavigating) return;
-  setIsNavigating(true);
+  const goToPrevSlide = () => {
+    if (isNavigating) return;
+    setIsNavigating(true);
 
-  if (currentNode && currentNode.prev) {
-    setCurrentNode(currentNode.prev);
-  } else {
-    setCurrentNode(doublyLinkedList.current.tail);
-  }
+    if (currentNode && currentNode.prev) {
+      setCurrentNode(currentNode.prev);
+    } else {
+      setCurrentNode(doublyLinkedList.current.tail);
+    }
 
-  setIsNavigating(false);
-};
+    setIsNavigating(false);
+  };
 
-const getSlideStyle = (node, currentNode) => {
-  if (node === currentNode) {
-    return { transform: "translateX(0)", zIndex: 2 };
-  } else if (
-    node === currentNode.prev ||
-    (node.next === currentNode && node.prev === null)
-  ) {
-    return { transform: "translateX(-100%)", zIndex: 1 };
-  } else if (
-    node === currentNode.next ||
-    (node.prev === currentNode && node.next === null)
-  ) {
-    return { transform: "translateX(100%)", zIndex: 1 };
-  } else {
-    return { transform: "translateX(-300%)" }; // Move the other slides off-screen
-  }
-};
+  const getSlideStyle = (node, currentNode) => {
+    if (node === currentNode) {
+      return { transform: "translateX(0)", zIndex: 2 };
+    } else if (
+      node === currentNode.prev ||
+      (node.next === currentNode && node.prev === null)
+    ) {
+      return { transform: "translateX(-100%)", zIndex: 1 };
+    } else if (
+      node === currentNode.next ||
+      (node.prev === currentNode && node.next === null)
+    ) {
+      return { transform: "translateX(100%)", zIndex: 1 };
+    } else {
+      return { transform: "translateX(-300%)" }; // Move the other slides off-screen
+    }
+  };
 
-const renderSlides = () => {
-  let slidesToRender = [];
-  let currentNodeToRender = doublyLinkedList.current.head;
-  let index = 0;
+  const renderSlides = () => {
+    let slidesToRender = [];
+    let currentNodeToRender = doublyLinkedList.current.head;
+    let index = 0;
 
-  if (currentNodeToRender) {
-    do {
-      let style = getSlideStyle(currentNodeToRender, currentNode);
+    if (currentNodeToRender) {
+      do {
+        let style = getSlideStyle(currentNodeToRender, currentNode);
 
-      slidesToRender.push(
-        <div
-          key={`${currentNodeToRender.data.title}-${index}`}
-          className="carousel-item"
-          style={style}
-        >
-          <img
-            src={currentNodeToRender.data.image}
-            alt={currentNodeToRender.data.title}
-          />
-          <h3>{currentNodeToRender.data.title}</h3>
-          <p>{currentNodeToRender.data.description}</p>
-        </div>
-      );
-      currentNodeToRender = currentNodeToRender.next;
-      index++;
-    } while (currentNodeToRender !== doublyLinkedList.current.head);
-  }
+        slidesToRender.push(
+          <div
+            key={`${currentNodeToRender.data.title}-${index}`}
+            className="carousel-item"
+            style={style}
+          >
+            <img
+              src={currentNodeToRender.data.image}
+              alt={currentNodeToRender.data.title}
+            />
+            <h3>{currentNodeToRender.data.title}</h3>
+            <p>{currentNodeToRender.data.description}</p>
+          </div>
+        );
+        currentNodeToRender = currentNodeToRender.next;
+        index++;
+      } while (currentNodeToRender !== doublyLinkedList.current.head);
+    }
 
-  return slidesToRender;
-};
-
+    return slidesToRender;
+  };
 
   return (
     <div className="carousel-wrapper">
