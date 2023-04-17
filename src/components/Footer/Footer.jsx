1230dd7fa7f02fbce29005fc319947e5a1c37be7
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-17 10:53:45 -0700

    add README file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,15 @@
 import githubLogo from "../../assets/githubLogo.png";
 import linkedinLogo from "../../assets/linkedinLogo.png";
-import personalLogo from "../../assets/personalLogo.png"
-import "./Footer.css"
+import personalLogo from "../../assets/personalLogo.png";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
       <section className="footer-info">
-        <div className="footer-text">
-          {/* <h3>Evgenii Shvetsov</h3> */}
-          {/* <a
-            href="https://shvetsov.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit Portfolio Website
-          </a> */}
-        </div>
-
         <div className="social_media">
           <a
-            href="https://www.linkedin.com/in/shvetsovea/"
+            href="https://github.com/evgenii-shvetsov/react-slider/tree/main"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -34,13 +23,12 @@ const Footer = () => {
             <img src={personalLogo} alt="personal website logo" />
           </a>
           <a
-            href="https://github.com/evgenii-shvetsov"
+            href="https://www.linkedin.com/in/shvetsovea/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src={linkedinLogo} alt="linkedin logo" />
           </a>
-          
         </div>
       </section>
     </footer>
